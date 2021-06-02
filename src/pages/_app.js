9222ca1ca0ba543424684b20c68e40b77c4af05a
hirenc-sixberries <hirenchavda@sixberries.com>
2021-06-02 19:06:48 +0530

    added the files

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,7 +1,7 @@
 import AppLayout from '../components/core/AppLayout/AppLayout';
 import PropTypes from 'prop-types';
-// import 'react-toastify/dist/ReactToastify.css';
-//import 'styles/globals.scss';
+import 'react-toastify/dist/ReactToastify.css';
+import 'styles/globals.scss';
 
 const MyApp = ({ Component, pageProps }) => (
   <AppLayout>
---
+++ src/tests/components/common/Forms/Button.test.js
@@ -0,0 +1,12 @@
+import { shallow } from 'enzyme';
+
+import Button from '../../../../components/common/Forms/Button';
+
+/** @test {Button Component} */
+describe('Button Component', () => {
+  it('should render without crashing', () => {
+    const wrapper = shallow(<Button  />);
+
+    expect(wrapper.find('div')).toHaveLength(1);
+  });
+});
