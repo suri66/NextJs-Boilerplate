a99c99e460eca155d232e434ebfad4ebdbe629e2
Surjeet Bhadauriya <sbsurjeet66@gmail.com>
2021-06-15 19:55:35 +0530

    redux has been added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,20 @@
 import AppLayout from '../components/core/AppLayout/AppLayout';
 import PropTypes from 'prop-types';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import { Provider, connect } from 'react-redux';
+import rootReducer from '../redux';
 import 'react-toastify/dist/ReactToastify.css';
 import 'styles/globals.scss';
 
+const store = createStore(rootReducer, applyMiddleware(thunk));
+
 const MyApp = ({ Component, pageProps }) => (
-  <AppLayout>
-    <Component {...pageProps} />
-  </AppLayout>
+  <Provider store={store}>
+    <AppLayout>
+      <Component {...pageProps} />
+    </AppLayout>
+  </Provider>
 );
 
 MyApp.propTypes = {
